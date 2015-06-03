6a294d78c1e0a834922d9fb6bc6797fd50c952bc
alvinkao <alvin.kao@gmail.com>
2015-06-03 17:32:40 +0800

    django_bookmarks

---
+++ django_bookmarks/django_bookmarks/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from bookmarks.views import *
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_bookmarks.views.home', name='home'),
+     url(r'^$', main_page),
+	 url(r'^admin/', include(admin.site.urls)),
+     url(r'^user/(\w+)/$',user_page),
+     url(r'^login/$', 'django.contrib.auth.views.login'),
+     )
